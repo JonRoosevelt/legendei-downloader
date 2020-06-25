092e0e15de62aae487577b946c00d0a3c0a228ce
JonRoosevelt <jonathanroosevelt@gmail.com>
2020-06-25 11:00:54 -0300

    Implements legendei-downloader

---
+++ index.js
@@ -0,0 +1,18 @@
+import { DownloadFile, getHTML, getSubtitleLink, GetUrl, GetFileName } from './lib/scraper';
+
+async function run() {
+    try {
+        const url = GetUrl();
+        const fileName = GetFileName(url);
+        const html = await getHTML(url)
+        console.log(`>>> Downloading ${url.replace('.zip','')} subtitle from ${url}`);
+        console.log('\n. . .\n');
+        const link = await getSubtitleLink(html)
+        await DownloadFile(link, fileName);
+        console.log(`< Subtitle ${url.replace('.zip', '')} sucessfully downloaded.`);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+run();
